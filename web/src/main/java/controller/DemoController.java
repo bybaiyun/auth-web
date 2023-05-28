7ac2ca005d39d20c9b073e0402cf804f3a1ae2ac
baiyun <carrot1631@gmail.com>
2023-05-28 20:51:20 +0800

    bugfix

--- web/src/main/java/controller/DemoController.java
+++ web/src/main/java/controller/DemoController.java
@@ -1,11 +1,11 @@
 package controller;
 
-import com.example.demo.service.DemoService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import service.DemoService;
 
 @Slf4j
 @RestController
